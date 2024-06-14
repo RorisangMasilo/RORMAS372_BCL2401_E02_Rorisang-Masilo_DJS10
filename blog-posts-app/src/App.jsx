29a218b0bd792f661921bdeb63e74c104b266e59
Rori <rbmasilo@gmail.com>
2024-06-14 09:44:19 +0200

    Imports: Import React, useEffect, useState. State management: Declarations, of posts and errors, to store list of blog posts and error messages

--- blog-posts-app/src/App.jsx
+++ blog-posts-app/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, sePosts] = useState([]);
+  const [error, setError] = useState(null);
 
+  useEffect (())
   return (
     <>
       <div>
